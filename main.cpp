aea7097620819facd4f6f2df77424dc3f718993c
Hoang Vien <80262438+N3iV@users.noreply.github.com>
2021-10-27 16:54:20 +0700

    hello

--- main.cpp
+++ main.cpp
@@ -1,32 +1,32 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <math.h>
-#include"loginManager.h"
-using namespace std;
-int main()
-{
-    QuanLyDangNhap app;
-    int option;
-    bool isLogin;
-    cout<<"Nhap lua chon"<<endl;
-    cout<<"1.Dang ky"<<endl;
-    cout<<"2.Dang nhap"<<endl;
-    cin>>option;
-    switch(option){
-        case 1: 
-            {
-                app.dangKi();
-                break;
-            }
+// #include <iostream>
+// #include <fstream>
+// #include <string>
+// #include <math.h>
+// #include"loginManager.h"
+// using namespace std;
+// int main()
+// {
+//     QuanLyDangNhap app;
+//     int option;
+//     bool isLogin;
+//     cout<<"Nhap lua chon"<<endl;
+//     cout<<"1.Dang ky"<<endl;
+//     cout<<"2.Dang nhap"<<endl;
+//     cin>>option;
+//     switch(option){
+//         case 1: 
+//             {
+//                 app.dangKi();
+//                 break;
+//             }
          
-        case 2:    
-            {
-                isLogin = app.dangNhap();
-                break;
-            }
-    }
-    if(isLogin){
-        cout<<"Den voi phan quan ly ne";
-    }
-}
+//         case 2:    
+//             {
+//                 isLogin = app.dangNhap();
+//                 break;
+//             }
+//     }
+//     if(isLogin){
+//         cout<<"Den voi phan quan ly ne";
+//     }
+// }
